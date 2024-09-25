e3988c000b5ccc0b31d4dd4ee89414170ec5f525
obiohaa <obiohaebuka200215@gmail.com>
2024-09-25 12:00:53 +0100

    hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,17 @@
 function Hero() {
   return (
-    <>
-      <h1>hellllo</h1>
-    </>
+    <section className="hero" id="home">
+      <div className="hero-banner">
+        <h1>backroads app</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae explicabo debitis est autem
+          dicta.
+        </p>
+        <a href="#tours" className="btn hero-btn ">
+          explore tours
+        </a>
+      </div>
+    </section>
   );
 }
 export default Hero;
